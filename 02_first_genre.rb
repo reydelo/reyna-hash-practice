34b1dec34cbabfdb6423b4eb5cbe21cea9a7efb6
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-11 09:27:07 -0700

    print the first musical genre that each person listed

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+puts PEOPLE['joe'][:preferences][:favorite_genres].first
+puts PEOPLE['sue'][:preferences][:favorite_genres].first
